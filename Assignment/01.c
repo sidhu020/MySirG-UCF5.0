a00687daabe51139c202cf2f543f833732116c38
BORISAGAR SIDDHARTH <115538682+sidhu020@users.noreply.github.com>
2025-02-18 13:48:00 +0530

    Create 01.c

---
+++ Assignment/01.c
@@ -0,0 +1,18 @@
+/*
+ASsignmetn-01-Input and output in C Language
+01-Write a program to print Hello Students on the screen.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+printf("Hello Studennts");
+
+return 0;
+}
+/*
+Output:-
+Hello Studennts
+*/
